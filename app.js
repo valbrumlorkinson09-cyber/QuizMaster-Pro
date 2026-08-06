1e080fc4bfce23f99c23b4aca78024d1352b3d5e
valbrumlorkinson09-cyber <valbrumlorkinson09@gmail.com>
2026-08-06 11:14:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -35,3 +35,61 @@ const questions = [
 
 let currentQuestion = 0;
 let score = 0;
+
+const playBtn = document.getElementById("playBtn");
+
+playBtn.addEventListener("click", startQuiz);
+
+
+function startQuiz(){
+  currentQuestion = 0;
+  score = 0;
+
+  showQuestion();
+}
+
+
+function showQuestion(){
+
+  const q = questions[currentQuestion];
+
+  alert(
+    "K kestyon: " + q.question +
+    "\n\n1️⃣ " + q.answers[0] +
+    "\n2️⃣ " + q.answers[1] +
+    "\n3️⃣ " + q.answers[2] +
+    "\n4️⃣ " + q.answers[3]
+  );
+
+  let answer = prompt("Chwazi repons lan (1-4)");
+
+  if(answer - 1 === q.correct){
+    score++;
+    alert("✅ Bon repons!");
+  }else{
+    alert("❌ Move repons!");
+  }
+
+
+  currentQuestion++;
+
+  if(currentQuestion < questions.length){
+    showQuestion();
+  }else{
+    finishQuiz();
+  }
+
+}
+
+
+function finishQuiz(){
+
+  alert(
+    "🏆 Quiz fini!\n\nOu fè: " +
+    score +
+    "/" +
+    questions.length +
+    " pwen"
+  );
+
+}
